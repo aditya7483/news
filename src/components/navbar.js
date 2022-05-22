5a9473dcdec6c587efb243bd60d092525c554cf1
Aditya <adityanarayanjha31@gmail.com>
2022-05-22 15:02:48 +0530

    changed name

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -26,7 +26,7 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Navbar</Link>
+          <Link className="navbar-brand" to="/">News74</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
